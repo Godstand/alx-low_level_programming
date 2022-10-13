9fd9e50f248eb87b0fb41f7e49381b37ae7d8467
Godstand <godstand1@gmail.com>
2022-10-13 19:51:46 -0400

    k

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char)\n);
+printf("Size of an int: %d byte(s)\n", sizeof(int)\n);
+printf("Size of a long int: %d byte(s)\n", sizeof(long int)\n);
+printf("Size of a long long: %d byte(s)\n", sizeof(long long)\n);
+printf("Size of a float: %d byte(s)\n", sizeof(float)\n);
+
+return (0);
+}
