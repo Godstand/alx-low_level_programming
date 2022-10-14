c9c747fc8ba0fea337709909fba994ad8f66ab69
Godstand <godstand1@gmail.com>
2022-10-14 10:06:18 -0400

    H

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -11,6 +13,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
+printf("n: ");
 scanf("%d", &n);
 if (n > 0)
 {
