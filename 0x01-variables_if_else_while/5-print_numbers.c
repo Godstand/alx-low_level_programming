f5dc38600e350b7aeccadbe08926fcee12523405
Godstand <godstand1@gmail.com>
2022-10-14 16:59:00 -0400

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c;
+
+for (c = 1; c <= 10; c++)
+{
+printf("%d", c);
+}
+printf("\n");
+return (0);
+}
