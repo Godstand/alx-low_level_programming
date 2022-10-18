fd7bad7ed160fb90f34cb9869828a32007f52cff
Godstand <godstand1@gmail.com>
2022-10-18 11:58:01 -0400

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
     _putchar('_');
@@ -16,7 +16,6 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-    
 	return (0);
 
 }
