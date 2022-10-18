22a10c9179cacf7085f6263761f38d3d4e3f939c
Godstand <godstand1@gmail.com>
2022-10-18 18:27:57 -0400

    k

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+        _putchar(98);
 		return (1);
 	}
 	else if (n < 0)
 	{
+        _putchar(32);
 		return (-1);
 	}
 	else
 	{
+        _putchar(100);
 		return (0);
 	}
 	_putchar('\n');
