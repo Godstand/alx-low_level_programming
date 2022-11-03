57c4ebe7253718bd6c9a20c1a2951cdbcc3cb705
Godstand <godstand1@gmail.com>
2022-11-03 11:12:22 -0400

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string recursively.
+ * _puts_recursion - Prints a string recursively followed by a new line.
  * @s: string.
  */
 void _puts_recursion(char *s)
 {
-	while (*s)
-		_putchar(*s++);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
