e46c8d2446022b0d42199f34808243e77b84aa3f
Godstand <godstand1@gmail.com>
2022-11-04 00:55:35 -0400

    factorial

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,4 +17,7 @@ void print_times_table(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+
 #endif
